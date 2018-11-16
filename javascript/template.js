7f42a5e27ad2c234565e826bb5f40c8a8fc4d4ca
Mica Gardner <mgardner2020@u.northwestern.edu>
2018-11-15 19:54:55 -0600

    Added content to Community page & formatted it.  Changes to template.js.

--- javascript/template.js
+++ javascript/template.js
@@ -15,7 +15,7 @@ let navContent= `<ul>
 </ul>
 `;
 
-let footerContent= `<a href= "https://www.facebook.com/immigrantsandwich/?hc_ref=ARTxAhBZs3E2KV7mUAvh3QJxdRXtIpwGAX2DeyGGkTLkc3SIQxsQjJJdGuas2ngPjBI&__xts__%5B0%5D=68.ARAR1RKSlJ1NL0vkE8N4qxST-DNOLqTGXm5vRSX7Fmb90GnV6ik_tv7I_PyiXohKpg4tCKaSU9dkttKIQECuR1joJMU-0NauLM825Jb_5rMJzhjEit4_RKX48qf2ZJHl5dheblbk2_-nvEBhy4DIJtgx5fOYJVKFyxbWjgdez6xJsyJiruqbTDkVsu06d-HMfMWy8Wcz4Rqmq24wUciidHJp2r5cyIkiWTeMmY4ktJXdzRxna969zGEPsqeHOPo6kuXv-PHwNrsYYgrfqQYP0luFsNGX0RP-la0YD2LFGI3rYIOIeIAuMAyaGOoJonajqNNHbxSuWGydA9J7vHhHPP1gu-2xPzqcFfVccPTF3g0&__tn__=kC-R"><img class="facebookLogo" src="/images/facebook.png" alt="Facebook Icon" width="50px">`
+let footerContent= `<a href= "https://www.facebook.com/immigrantsandwich/?hc_ref=ARTxAhBZs3E2KV7mUAvh3QJxdRXtIpwGAX2DeyGGkTLkc3SIQxsQjJJdGuas2ngPjBI&__xts__%5B0%5D=68.ARAR1RKSlJ1NL0vkE8N4qxST-DNOLqTGXm5vRSX7Fmb90GnV6ik_tv7I_PyiXohKpg4tCKaSU9dkttKIQECuR1joJMU-0NauLM825Jb_5rMJzhjEit4_RKX48qf2ZJHl5dheblbk2_-nvEBhy4DIJtgx5fOYJVKFyxbWjgdez6xJsyJiruqbTDkVsu06d-HMfMWy8Wcz4Rqmq24wUciidHJp2r5cyIkiWTeMmY4ktJXdzRxna969zGEPsqeHOPo6kuXv-PHwNrsYYgrfqQYP0luFsNGX0RP-la0YD2LFGI3rYIOIeIAuMAyaGOoJonajqNNHbxSuWGydA9J7vHhHPP1gu-2xPzqcFfVccPTF3g0&__tn__=kC-R"><img class="facebookLogo" src="images/facebook.png" alt="Facebook Icon" width="50px">`
 
 document.querySelector('.MainNavigation').innerHTML = navContent;
 document.querySelector('footer').innerHTML = footerContent;
