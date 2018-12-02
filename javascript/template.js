9163ff8ef969490c952017b426570c95e36d074e
ndouglas <44378635+natashadouglas@users.noreply.github.com>
2018-12-02 04:59:01 -0600

    Made various web page updates
    Added Google Quicksand font family throughout website; added functionality to nav bar (hover over, contact us); linked Immigrant Sandwich logo to index (home page); modified contact us link in footer; updated contact us page with new photo and layout; removed underlining from all web page links; added internal links; reduced size social_media.jpg

--- javascript/template.js
+++ javascript/template.js
@@ -1,6 +1,5 @@
 let navContent= `<ul>
   <li><a href="index.html">Home</a></li>
-
     <li><div class="dropdown"><button onclick="myFunction()" class="dropbtn">About</button>
       <div id= "aboutDropdown" class="dropdown-content">
         <a href="About_Us.html">General</a>
@@ -9,16 +8,16 @@ let navContent= `<ul>
         <a href="community.html">Our Community</a>
       </div>
     </div></li>
-
-  <li><a href="events.html">Events</a></li>
+  <li><a href="events.html">Live Shows</a></li>
   <li><a href="media.html">Social Media</a></li>
+  <li><a href="contact.html">Contact Us</a></li>
 </ul>
-<img src="images/sandwich_banner.jpg"
-  alt="sandwich_banner" width="100%">
+<a href="index.html"><img src="images/sandwich_banner.jpg"
+  alt="sandwich_banner" width="100%"></a>
 `;
 
 
-let footerContent= `<div class="contactUs"><h4>Contact Us</h4><a href="contact.html">Click Here</a><br><a href= "https://www.facebook.com/immigrantsandwich"><img class="facebookLogo" src="images/facebook.png" alt="Facebook Icon" width="50px"></div>`
+let footerContent= `<div class="contactUs"><a href="contact.html"><h4>Contact Us</h4></a><br><a href= "https://www.facebook.com/immigrantsandwich"><img class="facebookLogo" src="images/facebook.png" alt="Facebook Icon" width="50px"></div>`
 
 document.querySelector('.MainNavigation').innerHTML = navContent;
 document.querySelector('footer').innerHTML = footerContent;
